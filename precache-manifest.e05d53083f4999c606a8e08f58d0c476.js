5482d946a33377dfc15d7d41a0ab7ac23ddde2f7
Igor Nascimento <igorln96@gmail.com>
2021-03-08 10:57:13 -0300

    Updates

---
+++ precache-manifest.e05d53083f4999c606a8e08f58d0c476.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069a89ea0b1200451589db2431b86632",
+    "url": "/app-toot-name/index.html"
+  },
+  {
+    "revision": "b40f07d0326e238cfc59",
+    "url": "/app-toot-name/static/css/main.38d6a80c.chunk.css"
+  },
+  {
+    "revision": "e1071e0db0fd88bdcb92",
+    "url": "/app-toot-name/static/js/2.be628a0f.chunk.js"
+  },
+  {
+    "revision": "b40f07d0326e238cfc59",
+    "url": "/app-toot-name/static/js/main.f73f7fb3.chunk.js"
+  },
+  {
+    "revision": "10f00aa2016367073b45",
+    "url": "/app-toot-name/static/js/runtime-main.fb5cccab.js"
+  }
+]);
