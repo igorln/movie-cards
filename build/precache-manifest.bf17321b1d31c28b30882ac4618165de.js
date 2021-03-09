ce1f05aadfb084e45bd09c266e6d9d92eaaa8c42
Igor Nascimento <igorln96@gmail.com>
2021-03-09 19:45:24 -0300

    update

---
+++ build/precache-manifest.bf17321b1d31c28b30882ac4618165de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "635ef694a49233eb061b548ecd1ed5db",
+    "url": "/movie-cards/index.html"
+  },
+  {
+    "revision": "052e48c8635642285251",
+    "url": "/movie-cards/static/css/main.38d6a80c.chunk.css"
+  },
+  {
+    "revision": "e1071e0db0fd88bdcb92",
+    "url": "/movie-cards/static/js/2.be628a0f.chunk.js"
+  },
+  {
+    "revision": "052e48c8635642285251",
+    "url": "/movie-cards/static/js/main.2c51c7ac.chunk.js"
+  },
+  {
+    "revision": "e0ba7f353d36a6504002",
+    "url": "/movie-cards/static/js/runtime-main.f38ae7b4.js"
+  }
+]);
